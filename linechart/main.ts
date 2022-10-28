987626aa76acb3adecce3f954c79fecc90f73385
jiangshuhe <shuhej@andrew.cmu.edu>
2022-10-28 15:00:19 -0700

    not null

--- linechart/main.ts
+++ linechart/main.ts
@@ -21,7 +21,7 @@ async function update(Station: string) {
   round(quantile_cont("US AQI", 0.9), 2) as AQI_2,
   FROM airquality.parquet
   WHERE "Station name" = '${Station}' 
-  AND AQI IS NOT NULL AND AQI_1 IS NOT NULL AND AQI_2 IS NOT NULL
+  AND "Station name" IS NOT NULL
   GROUP BY time 
   ORDER BY time`);
 
@@ -29,7 +29,7 @@ async function update(Station: string) {
   SELECT date_trunc('day', "Timestamp(UTC)") as time1, round("US AQI", 2) as AQI1
   FROM airquality.parquet
   WHERE "Station name" = '${Station}'
-  AND AQI IS NOT NULL AND AQI_1 IS NOT NULL AND AQI_2 IS NOT NULL
+  AND "Station name" IS NOT NULL
   GROUP BY time1, AQI1 
   ORDER BY time1`);
 
